aeedff50453d34253757fb17148553de25c555c8
R-Usuzumi <gnk49955@kwansei.ac.jp>
2024-11-02 01:14:23 +0900

    routのバグ修正，pdf名指定できるように実装

--- bin/rout.sh
+++ bin/rout.sh
@@ -2,7 +2,7 @@
 
 source ~/.zshrc
 
-#  設定ファイルを読み込み
+# 設定ファイルを読み込み
 config_file="$HOME/.rsemi_config"
 
 if [ -f "$config_file" ]; then
@@ -12,15 +12,33 @@ else
     exit 1
 fi
 
-html_file=$1
-if [[ "$html_file" != *.html && "$html_file" != *.htm ]]; then
-    echo "Error: The provided file is not in HTML format."
+output_pdf_name=""
+
+# オプション引数を追加できる，--name pdf_file名のようにする
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --name)
+            shift
+            output_pdf_name="$1"
+            ;;
+        *)
+            html_file="$1"
+            ;;
+    esac
+    shift
+done
+
+# HTMLファイルが指定されているかと拡張子の確認
+if [[ -z "$html_file" || ("$html_file" != *.html && "$html_file" != *.htm) ]]; then
+    echo "Error: Please provide an HTML file as input."
     exit 1
 fi
 
-file_name="${html_file##*/}"
+# 出力ファイル名の設定
+file_name="${html_file##*/}" 
 base_name="${file_name%.*}"
-pdf_file="$RSEMI_PATH/material/$base_name.pdf"
+pdf_file="$RSEMI_PATH/material/${output_pdf_name:-$base_name}.pdf"
 
-google-chrome --headless --print-to-pdf="$pdf_file" "$html_file"
+# HTMLをPDFに変換
+google-chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --virtual-time-budget=10000 --print-to-pdf="$pdf_file" "$html_file"
 open "$pdf_file"
